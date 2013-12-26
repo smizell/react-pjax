b045c8118963488b56340000912078939bc95ffb
Stephen Mizell <smizell@gmail.com>
2013-12-26 15:37:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReactPjax::Application.config.secret_key_base = '38f9d3e258904625f4fc20eeec4f0e5c697b2541c1840db1a8d227b4a8d513153c47c52a9c84a8f08e3561d2c473a09f4862520071ed636481a78907f942faf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReactPjax::Application.config.session_store :cookie_store, key: '_react-pjax_session'
